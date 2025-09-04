1c452c9f75b6c5765e6b24a3d9777871574dc1d0
facundovaras <facundovaras17@gmail.com>
2025-09-04 18:14:40 -0400

    feat: add CSS modules for buttons, cards and form components with responsive styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: [
+    '~/assets/css/theme.css',
+    '~/assets/css/buttons.css',
+    '~/assets/css/forms.css',
+    '~/assets/css/cards.css',
+    '~/assets/css/responsive.css'
+  ],
   modules: ['@nuxt/eslint', '@nuxt/icon'],
   ssr: true,
   plugins: [
